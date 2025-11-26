0faba599330682bed5c0e57643ad3d6f971e1742
knlnguyen1802 <knlnguyen1802@gmail.com>
2025-11-26 12:06:28 +0800

    Optimize encoder cache memory consumption by storing encoder outputs only
    Signed-off-by: knlnguyen1802 <knlnguyen1802@gmail.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -863,9 +863,9 @@ class Scheduler(SchedulerInterface):
         num_tokens_to_stored = 0
         for i, mm_feature in enumerate(mm_features):
             start_pos = mm_feature.mm_position.offset
-            # NOTE(Long) num_scheduler_encoder_tokens is used for 
+            # NOTE(Long) num_scheduler_encoder_tokens is used for
             # scheduling of both embed and non embed token
-            # num_stored_encoder_tokens is the actual number 
+            # num_stored_encoder_tokens is the actual number
             # of embed token stored in the encoder cache
             num_scheduler_encoder_tokens = mm_feature.mm_position.length
             num_stored_encoder_tokens = mm_feature.mm_position.get_num_embeds()
